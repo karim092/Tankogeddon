6974fc71aa883153e5b1f2d646e1165970b9861e
karim092 <71761680+karim092@users.noreply.github.com>
2022-05-17 10:47:20 +0300

    CreateProject

---
+++ Tankogeddon/Intermediate/Build/Win64/TankogeddonEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30141
+#include "D:/UE4_Curs/Tankogeddon/Tankogeddon/Intermediate/Build/Win64/TankogeddonEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Tankogeddon/Intermediate/Build/Win64/UE4Editor/Inc/Tankogeddon/TankogeddonGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef TANKOGEDDON_TankogeddonGameModeBase_generated_h
+#error "TankogeddonGameModeBase.generated.h already included, missing '#pragma once' in TankogeddonGameModeBase.h"
+#endif
+#define TANKOGEDDON_TankogeddonGameModeBase_generated_h
+
+#define Tankogeddon_Source_Tankogeddon_TankogeddonGameModeBase_h_15_SPARSE_DATA
+#define Tankogeddon_Source_Tankogeddon_TankogeddonGameModeBase_h_15_RPC_WRAPPERS
+#define Tankogeddon_Source_Tankogeddon_TankogeddonGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define Tankogeddon_Source_Tankogeddon_TankogeddonGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesATankogeddonGameModeBase(); \
+	friend struct Z_Construct_UClass_ATankogeddonGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ATankogeddonGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Tankogeddon"), NO_API) \
+	DECLARE_SERIALIZER(ATankogeddonGameModeBase)
+
+
+#define Tankogeddon_Source_Tankogeddon_TankogeddonGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesATankogeddonGameModeBase(); \
+	friend struct Z_Construct_UClass_ATankogeddonGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ATankogeddonGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Tankogeddon"), NO_API) \
+	DECLARE_SERIALIZER(ATankogeddonGameModeBase)
+
+
+#define Tankogeddon_Source_Tankogeddon_TankogeddonGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATankogeddonGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATankogeddonGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATankogeddonGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATankogeddonGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATankogeddonGameModeBase(ATankogeddonGameModeBase&&); \
+	NO_API ATankogeddonGameModeBase(const ATankogeddonGameModeBase&); \
+public:
+
+
+#define Tankogeddon_Source_Tankogeddon_TankogeddonGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATankogeddonGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATankogeddonGameModeBase(ATankogeddonGameModeBase&&); \
+	NO_API ATankogeddonGameModeBase(const ATankogeddonGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATankogeddonGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATankogeddonGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATankogeddonGameModeBase)
+
+
+#define Tankogeddon_Source_Tankogeddon_TankogeddonGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define Tankogeddon_Source_Tankogeddon_TankogeddonGameModeBase_h_12_PROLOG
+#define Tankogeddon_Source_Tankogeddon_TankogeddonGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Tankogeddon_Source_Tankogeddon_TankogeddonGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Tankogeddon_Source_Tankogeddon_TankogeddonGameModeBase_h_15_SPARSE_DATA \
+	Tankogeddon_Source_Tankogeddon_TankogeddonGameModeBase_h_15_RPC_WRAPPERS \
+	Tankogeddon_Source_Tankogeddon_TankogeddonGameModeBase_h_15_INCLASS \
+	Tankogeddon_Source_Tankogeddon_TankogeddonGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Tankogeddon_Source_Tankogeddon_TankogeddonGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Tankogeddon_Source_Tankogeddon_TankogeddonGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Tankogeddon_Source_Tankogeddon_TankogeddonGameModeBase_h_15_SPARSE_DATA \
+	Tankogeddon_Source_Tankogeddon_TankogeddonGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	Tankogeddon_Source_Tankogeddon_TankogeddonGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	Tankogeddon_Source_Tankogeddon_TankogeddonGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> TANKOGEDDON_API UClass* StaticClass<class ATankogeddonGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Tankogeddon_Source_Tankogeddon_TankogeddonGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
